1c2f3a51b8698d18983c0188e206f4a3d10f3cf5
Ashutosh Mishra <ashutosh.mishra@greatwits.com>
2022-01-13 12:01:20 +0530

    first commit

---
+++ StaticEx.java
@@ -0,0 +1,23 @@
+package StaticDemo;
+
+public class StaticEx {
+
+   static int a=6;
+	public static void main(String[] args) {
+		
+       System.out.println("Main method ");
+         System.out.println(StaticEx.a);
+    StaticEx.m1();
+       
+	}
+     static void m1()
+     {
+    	 System.out.println("Inside  method ");
+     }
+    static{
+    	
+    	 System.out.println("this is  static block");
+    	 StaticEx.m1();
+    }
+   
+}
